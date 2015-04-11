d2538e626b5bda5b34b308c4ce467c59961f6380
Wei Ye <vejuhust@gmail.com>
2015-04-11 22:39:06 +0800

    Add alias of 'grep' for bash

--- bash/bash_aliases.sh
+++ bash/bash_aliases.sh
@@ -46,6 +46,10 @@ alias ...='cd ../../../'
 alias ....='cd ../../../../'
 alias .....='cd ../../../../../'
 
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias grep='grep --color=auto'
+
 alias l='ls -CF'
 alias la='ls -A'
 alias ll='ls -lashF'
